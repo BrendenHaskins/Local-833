37fd36e7138ac2347ab557e7eadc82d9574e2e9a
BrendenHaskins <brendenshaskins@gmail.com>
2024-04-22 01:45:27 -0400

    troubleshooting session variables

--- index.php
+++ index.php
@@ -40,17 +40,44 @@ $f3->route('GET|POST /personal', function($f3){
 
 $f3->route('GET|POST /experience', function($f3){
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $f3->set('SESSION.fname',$_POST['fname']);
-        $f3->set('SESSION.lname',$_POST['lname']);
-        $f3->set('SESSION.state',$_POST['state']);
-        $f3->set('SESSION.email',$_POST['email']);
-        $f3->set('SESSION.phone',$_POST['phone']);
+        $f3->set('SESSION.bio',$_POST['bio']);
+        $f3->set('SESSION.linkedIn',$_POST['linkedIn']);
+        $f3->set('SESSION.exp',$_POST['exp']);
+        $f3->set('SESSION.relocate',$_POST['relocate']);
 
-        $f3->reroute("experience");
+        $f3->reroute("openings");
     }else {
         $view = new Template();
         echo $view->render('views/experience.html');
     }
+});
+
+$f3->route('GET|POST /openings', function($f3){
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $f3->set('SESSION.positions',$_POST['positions']);
+
+        $f3->reroute("summary");
+    }else {
+        $view = new Template();
+        echo $view->render('views/openings.html');
+    }
+});
+
+$f3->route('GET|POST /summary', function($f3){
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $f3->set('SESSION.positions',$_POST['positions']);
+
+        $f3->reroute("summary");
+    }else {
+        $view = new Template();
+        echo $view->render('views/summary.html');
+    }
+});
+
+$f3->route('GET /home', function(){
+        $view = new Template();
+        echo $view->render('views/home.html');
+
 });
 //run fat free
 $f3->run();
