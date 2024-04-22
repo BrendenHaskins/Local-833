b8c42d76d275f9b51e661550d220cf828130cc87
BrendenHaskins <brendenshaskins@gmail.com>
2024-04-22 01:49:19 -0400

    final touches

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+//Brenden Haskins 4/21/2024 Scripts go here... I realize now I didn't need to do this. That's alright.
+
 function personalSubmit() {
     let form = document.getElementById('personalForm');
     form.submit();
